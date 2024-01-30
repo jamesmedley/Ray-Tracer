804ddfb31699ab411914a8e82ddaf89162d8c0b2
James Medley <81381425+jmed13@users.noreply.github.com>
2024-01-30 16:10:13 +0000

    more materials

--- src/tracer/Main.java
+++ src/tracer/Main.java
@@ -24,21 +24,19 @@ public class Main {
         Renderer renderer = new Renderer(Properties.WIDTH, Properties.HEIGHT);
         Scene scene = new Scene(Properties.AMBIENT);
 
-        scene.addPlane(new Vector(0, -500, 0), Materials.MIRROR, new Vector(0, 1, 0)); // Floor
-        scene.addPlane(new Vector(0, 500, 0), Materials.MIRROR, new Vector(0, -1, 0)); // Ceiling
-        scene.addPlane(new Vector(0, 0, 1000), Materials.MIRROR, new Vector(0, 0, -1)); // Back wall 
-        scene.addPlane(new Vector(500, 0, 0), Materials.materialForColour(new RGB(1,0,0)), new Vector(-1, 0, 0)); // Right wall
-        scene.addPlane(new Vector(-500, 0, 0), Materials.materialForColour(new RGB(0,1,0)), new Vector(1, 0, 0)); // left wall
+        scene.addPlane(new Vector(0, -500, 0), Materials.NEUTRAL, new Vector(0, 1, 0)); // Floor
+        //    scene.addPlane(new Vector(0, 500, 0), Materials.NEUTRAL, new Vector(0, -1, 0)); // Ceiling
+        //  scene.addPlane(new Vector(0, 0, 1000), Materials.MIRROR, new Vector(0, 0, -1)); // Back wall 
+        //   scene.addPlane(new Vector(0, 0, -1300), Materials.MIRROR, new Vector(0, 0, 1)); // Front wall 
+        //   scene.addPlane(new Vector(500, 0, 0), Materials.materialForColour(new RGB(1,0,0)), new Vector(-1, 0, 0)); // Right wall
+        //  scene.addPlane(new Vector(-500, 0, 0), Materials.materialForColour(new RGB(0,1,0)), new Vector(1, 0, 0)); // left wall
 
-        scene.addSphere(new Vector(-150, -300, 300), Materials.POLISHED_SILVER,200); // Polished Silver
-        scene.addSphere(new Vector(300, -400, 200), Materials.materialForColour(new RGB(0,1,0)),100); // Green
+        scene.addSphere(new Vector(-150, -300, 300), Materials.GOLD, 200);
+        scene.addSphere(new Vector(300, -400, 200), Materials.MIRROR, 100);
 
-
-        scene.addLight(new Vector(499, 499, 999), new RGB(1, 1,1), 2);
-        scene.addLight(new Vector(-499, 499, 999), new RGB(1, 1, 1), 2);
-        scene.addLight(new Vector(0, 499, 0), new RGB(1,1, 1), 2);
-        
-
+        scene.addLight(new Vector(499, 499, 999), new RGB(1, 0, 0), 2);
+        scene.addLight(new Vector(-499, 499, 999), new RGB(0, 1, 0), 2);
+        scene.addLight(new Vector(0, 499, 0), new RGB(0, 0, 1), 2);
 
         Tracer tracer = new Tracer(renderer);
         tracer.traceImage(scene, Properties.SAMPLES_PER_PIXEL);
@@ -63,7 +61,7 @@ public class Main {
             }
         }
         try {
-            File outputFile = new File("src/images/test.png");
+            File outputFile = new File("src/images/test3.png");
             ImageIO.write(bufferedImage, "png", outputFile);
             System.out.println("Image saved successfully!");
         } catch (IOException e) {
--- src/tracer/Materials.java
+++ src/tracer/Materials.java
@@ -8,6 +8,9 @@ import maths.Vector;
  */
 public class Materials {
 
+    /*
+        Materials sourced from: https://globe3d.sourceforge.io/g3d_html/gl-materials__ads.htm#26_5
+     */
     public static Material MIRROR = new Material(
             new RGB(1, 1, 1), // colour
             new Vector(0.1, 0.1, 0.1), // ambient
@@ -44,6 +47,87 @@ public class Materials {
             28 // shininess
     );
 
+    public static Material BRONZE = new Material(
+            new RGB(1, 1, 1), // colour
+            new Vector(0.2125, 0.1275, 0.054), // ambient
+            new Vector(0.714, 0.4284, 0.18144), // diffuse
+            new Vector(0.393548, 0.271906, 0.166721), // specular
+            new Vector(0, 0, 0), // emissive
+            26 // shininess
+    );
+
+    public static Material POLISHED_BRONZE = new Material(
+            new RGB(1, 1, 1), // colour
+            new Vector(0.25, 0.148, 0.06475), // ambient
+            new Vector(0.4, 0.2368, 0.1036), // diffuse
+            new Vector(0.774597, 0.774597, 0.774597), // specular
+            new Vector(0, 0, 0), // emissive
+            77 // shininess
+    );
+
+    public static Material CHROME = new Material(
+            new RGB(1, 1, 1), // colour
+            new Vector(0.25, 0.25, 0.25), // ambient
+            new Vector(0.4, 0.4, 0.4), // diffuse
+            new Vector(0.774597, 0.774597, 0.774597), // specular
+            new Vector(0, 0, 0), // emissive
+            77 // shininess
+    );
+
+    public static Material COPPER = new Material(
+            new RGB(1, 1, 1), // colour
+            new Vector(0.19125, 0.0735, 0.0225), // ambient
+            new Vector(0.7038, 0.27048, 0.0828), // diffuse
+            new Vector(0.256777, 0.137622, 0.086014), // specular
+            new Vector(0, 0, 0), // emissive
+            13 // shininess
+    );
+
+    public static Material POLISHED_COPPER = new Material(
+            new RGB(1, 1, 1), // colour
+            new Vector(0.2295, 0.08825, 0.0275), // ambient
+            new Vector(0.5508, 0.2118, 0.066), // diffuse
+            new Vector(0.580594, 0.223257, 0.0695701), // specular
+            new Vector(0, 0, 0), // emissive
+            51 // shininess
+    );
+
+    public static Material GOLD = new Material(
+            new RGB(1, 1, 1), // colour
+            new Vector(0.24725, 0.1995, 0.0745), // ambient
+            new Vector(0.75164, 0.60648, 0.22648), // diffuse
+            new Vector(0.628281, 0.555802, 0.366065), // specular
+            new Vector(0, 0, 0), // emissive
+            51 // shininess
+    );
+
+    public static Material POLISHED_GOLD = new Material(
+            new RGB(1, 1, 1), // colour
+            new Vector(0.24725, 0.2245, 0.0645), // ambient
+            new Vector(0.34615, 0.3143, 0.0903), // diffuse
+            new Vector(0.797357, 0.723991, 0.208006), // specular
+            new Vector(0, 0, 0), // emissive
+            83 // shininess
+    );
+
+    public static Material PEWTER = new Material(
+            new RGB(1, 1, 1), // colour
+            new Vector(0.105882, 0.058824, 0.113725), // ambient
+            new Vector(0.427451, 0.470588, 0.541176), // diffuse
+            new Vector(0.333333, 0.333333, 0.521569), // specular
+            new Vector(0, 0, 0), // emissive
+            10 // shininess
+    );
+
+    public static Material SILVER = new Material(
+            new RGB(1, 1, 1), // colour
+            new Vector(0.19225, 0.19225, 0.19225), // ambient
+            new Vector(0.50754, 0.50754, 0.50754), // diffuse
+            new Vector(0.508273, 0.508273, 0.508273), // specular
+            new Vector(0, 0, 0), // emissive
+            51 // shininess
+    );
+
     public static Material POLISHED_SILVER = new Material(
             new RGB(1, 1, 1), // colour
             new Vector(0.23125, 0.23125, 0.23125), // ambient
@@ -51,6 +135,78 @@ public class Materials {
             new Vector(0.773911, 0.773911, 0.773911), // specular
             new Vector(0, 0, 0), // emissive
             90 // shininess
+    );
+
+    public static Material EMERALD = new Material(
+            new RGB(1, 1, 1), // colour
+            new Vector(0.0215, 0.1745, 0.0215), // ambient
+            new Vector(0.07568, 0.61424, 0.07568), // diffuse
+            new Vector(0.633, 0.727811, 0.633), // specular
+            new Vector(0, 0, 0), // emissive
+            77 // shininess
+    );
+
+    public static Material JADE = new Material(
+            new RGB(1, 1, 1), // colour
+            new Vector(0.135, 0.2225, 0.1575), // ambient
+            new Vector(0.54, 0.89, 0.63), // diffuse
+            new Vector(0.316228, 0.316228, 0.316228), // specular
+            new Vector(0, 0, 0), // emissive
+            13 // shininess
+    );
+
+    public static Material OBSIDIAN = new Material(
+            new RGB(1, 1, 1), // colour
+            new Vector(0.05375, 0.05, 0.06625), // ambient
+            new Vector(0.18275, 0.17, 0.22525), // diffuse
+            new Vector(0.332741, 0.328634, 0.346435), // specular
+            new Vector(0, 0, 0), // emissive
+            38 // shininess
+    );
+
+    public static Material PEARL = new Material(
+            new RGB(1, 1, 1), // colour
+            new Vector(0.25, 0.20725, 0.20725), // ambient
+            new Vector(1, 0.829, 0.829), // diffuse
+            new Vector(0.296648, 0.296648, 0.296648), // specular
+            new Vector(0, 0, 0), // emissive
+            11 // shininess
+    );
+
+    public static Material RUBY = new Material(
+            new RGB(1, 1, 1), // colour
+            new Vector(0.1745, 0.01175, 0.01175), // ambient
+            new Vector(0.61424, 0.04136, 0.04136), // diffuse
+            new Vector(0.727811, 0.626959, 0.626959), // specular
+            new Vector(0, 0, 0), // emissive
+            77 // shininess
+    );
+
+    public static Material TURQUOISE = new Material(
+            new RGB(1, 1, 1), // colour
+            new Vector(0.1, 0.18725, 0.1745), // ambient
+            new Vector(0.396, 0.74151, 0.69102), // diffuse
+            new Vector(0.297254, 0.30829, 0.306678), // specular
+            new Vector(0, 0, 0), // emissive
+            13 // shininess
+    );
+
+    public static Material BLACK_PLASTIC = new Material(
+            new RGB(1, 1, 1), // colour
+            new Vector(0, 0, 0), // ambient
+            new Vector(0.01, 0.01, 0.01), // diffuse
+            new Vector(0.50, 0.50, 0.50), // specular
+            new Vector(0, 0, 0), // emissive
+            32 // shininess
+    );
+
+    public static Material BLACK_RUBBER = new Material(
+            new RGB(1, 1, 1), // colour
+            new Vector(0.02, 0.02, 0.02), // ambient
+            new Vector(0.01, 0.01, 0.01), // diffuse
+            new Vector(0.4, 0.4, 0.4), // specular
+            new Vector(0, 0, 0), // emissive
+            10 // shininess
     );
 
     public static Material materialForColour(RGB colour) {
